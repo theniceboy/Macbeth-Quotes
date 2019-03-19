3d3cbc837d346d13e46a417a60491ed132deb021
David Chen <chenwei2000121@163.com>
2019-03-19 14:32:07 -0700

    Initial Commit

---
+++ Macbeth Quotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Macbeth Quotes
+//
+//  Created by David Chen on 3/19/19.
+//  Copyright © 2019 David Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
